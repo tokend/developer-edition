3497325dd14aeefeb8405ee32d4108ed662deba9
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2018-10-19 00:49:10 +0300

    Storage service

--- configs/client.js
+++ configs/client.js
@@ -1,6 +1,6 @@
 document.ENV = {
     HORIZON_SERVER: 'http://localhost:8000',
-    FILE_STORAGE: 'http://dev.null',
+    FILE_STORAGE: 'http://localhost:9000/api',
     NETWORK_PASSPHRASE: 'TokenD Developer Network',
     KEY_SERVER_ADMIN: 'http://localhost:8006',
     VALIDATE_EMAILS: false,
