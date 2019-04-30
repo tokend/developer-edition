610bd549213bce46b1bdc1c509478d41cdc1f262
Pavel Ponomarev <i.haz.teh.codez@gmail.com>
2019-04-30 18:36:24 +0300

    Identity verification tasks

--- terraform/key_values.tf
+++ terraform/key_values.tf
@@ -15,3 +15,21 @@ resource tokend_key_value "asset_type_default" {
     value_type = "uint32"
     value = "${var.asset_type_default}"
 }
+
+resource tokend_key_value "change_role_task-submit_auto_verification" {
+    key = "change_role_task:submit_auto_verification"
+    value_type = "uint32"
+    value = "1024"
+}
+
+resource tokend_key_value "change_role_task-complete_auto_verification" {
+    key = "change_role_task:complete_auto_verification"
+    value_type = "uint32"
+    value = "2048"
+}
+
+resource tokend_key_value "change_role_task-manual_review_required" {
+    key = "change_role_task:manual_review_required"
+    value_type = "uint32"
+    value = "4096"
+}
