aed7c22713d05d209c06fa9fc2449de24580a80a
Levchks <tg.levochko@gmail.com>
2019-07-03 14:07:11 +0300

    update dns and identity versions

---
+++ configs/conto.js
@@ -0,0 +1,9 @@
+document.ENV = {
+    HORIZON_SERVER: 'http://localhost:8000/_/api/',
+    FILE_STORAGE: 'http://localhost:8000/_/storage/api',
+    NETWORK_PASSPHRASE: 'TokenD Developer Network',
+    KEY_SERVER_ADMIN: 'http://localhost:8000/_/adks',
+    VALIDATE_EMAILS: false,
+    WEB_CLIENT_URL: 'http://localhost:8060',
+    MODULE_SCHEME_NAME: 'conto'
+}
