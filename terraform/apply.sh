9e3188e23ce89aa16500cfca7d91cda1751a2bbb
Anton <antonymartynov@gmail.com>
2019-11-01 13:40:34 +0200

    Added -refresh=false flag in terraform/apply.sh
    If -refresh=true, each run of initscripts will reset all manually
    changed values (e.g. key values) to defaults, set in the tf script.
    
    This is not a desired behaviour, we expect dev-edition to store
    our changes even after `docker-compose down && docker-compose up`

--- terraform/apply.sh
+++ terraform/apply.sh
@@ -3,4 +3,4 @@
 set -eux
 
 until terraform init /opt/config; do echo "Waiting for state store" && sleep 3; done
-until terraform apply -auto-approve -parallelism=30 /opt/config; do echo "Waiting for network convergence" && sleep 3; done
+until terraform apply -auto-approve -parallelism=30 -refresh=false /opt/config; do echo "Waiting for network convergence" && sleep 3; done
