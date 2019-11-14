5b87d0fb039c440d123bc1b3c73ec56a9f8c9be7
Tatiana Levochko <tg.levochko@gmail.com>
2019-11-14 15:01:56 +0200

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,7 +14,7 @@ terraform {
 provider "tokend" {
   account  = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
   signer   = "SAMJKTZVW5UOHCDK5INYJNORF2HRKYI72M5XSZCBYAHQHR34FFR4Z6G4"
-  endpoint = "http://janus"
+  endpoint = "http://horizon"
 }
 
 module "tokend_vanilla" {
