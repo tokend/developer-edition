f5295177997485ea230e81c1767315b9e2c9708d
Sergey Gimonov <xr9kayu@gmail.com>
2020-08-11 18:37:05 +0300

    update images; terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket = "tfstate"
-    key    = "terraform.tfstate"
-    region = "us-east-1"
-    access_key = "miniominio"
-    secret_key = "sekritsekrit"
-    endpoint = "http://storage:9000"
-    skip_credentials_validation = true
-    force_path_style = true
-  }
-}
-
 provider "tokend" {
   account  = "GBA4EX43M25UPV4WIE6RRMQOFTWXZZRIPFAI5VPY6Z2ZVVXVWZ6NEOOB"
   signer   = "SAMJKTZVW5UOHCDK5INYJNORF2HRKYI72M5XSZCBYAHQHR34FFR4Z6G4"
@@ -18,5 +5,5 @@ provider "tokend" {
 }
 
 module "tokend_vanilla" {
-  source = "git::https://github.com/tokend/terraform-tokend-vanilla.git?ref=3919138573a9f9b330c6cda4ff000b701aecc279"
+  source = "git::https://github.com/tokend/terraform-tokend-vanilla.git?ref=project/blockparty"
 }
